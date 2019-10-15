5d9ae566c1537e0820f9560d37abdce90b89596f
Derbes <uderbes@gmail.com>
2019-10-15 17:38:19 +0600

    feat(not-found page): add not found

--- src/components/pages/NotFoundPage/NotFoundPage.js
+++ src/components/pages/NotFoundPage/NotFoundPage.js
@@ -1,11 +1,17 @@
 import React, { Component } from 'react'
-
+import smile from '../../../assets/images/404.png';
 export default class NotFoundPage extends Component {
     render() {
         return (
-            <div>
-                
+          <div className="NotFound">
+            <div className="NotFound__content">
+              <div className="NotFound__404">
+                4
+                <img className="NotFound__image" src={smile} alt="smile" />4
+              </div>
+              <div className="NotFound__content--title">Упс... что то пошло не так</div>
             </div>
-        )
-    }
+          </div>
+        );
+      }
 }
